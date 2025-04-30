2799b3680875e56cd898001c6bb9f48a5649b0db
Alejandragarciadominguez34 <alejandragado@gmail.com>
2025-04-30 12:24:42 +0000

    Código base, vosotros operaciones.

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print ("Hello World")
+# Codigo base lo hacen la coordinadora (alejandra)
+print ("Bienvenido a la calculadora de Youssef, Houda, María y Alejandra")
+
+Variable1 = int(input("Ingrese el primer número: "))
+Variable2 =  int(input("Ingrese el segundo número: "))
+Cuenta = input("Ingrese la operación a realizar (+, -, *, /): ")
+
+# Operaciones las hacen los desarrolladores (maria, youssef y houda)
+
+
+
+
+
+
+
+
+
+
+
+
+# Revisión la hace la coordinadora (alejandra)
